926f93962eb464c4327087ff2d8619796f04fc66
Dylan Gee <dylangee@gmail.com>
2022-11-30 22:01:44 +0000

    register route added

--- server/server.js
+++ server/server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
-const PORT = 5000 
+const PORT = 5000;
+const registerRouter = require('./routes/register');
 
-
+//Routes
+app.use('/register', registerRouter);
 
 app.listen(PORT, () => {
     console.log(`Listening on port: ${PORT}`)
