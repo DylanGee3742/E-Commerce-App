933ba8329409e8ec27dd0ef738aa2693e9aafe80
Dylan Gee <dylangee@gmail.com>
2022-12-01 19:11:07 +0000

    added cors and json parse

--- server/server.js
+++ server/server.js
@@ -2,9 +2,15 @@ const express = require('express');
 const app = express();
 const PORT = 5000;
 const registerRouter = require('./routes/register');
+const cors = require('cors');
 
+//Allow server to take JSON
+app.use(express.json());
 //Allow info to be sent from front to backend
-app.use(express.urlencoded({ extended: false }))
+app.use(express.urlencoded({ extended: false }));
+
+
+app.use(cors());
 
 //Routes
 app.use('/register', registerRouter);
@@ -12,3 +18,4 @@ app.use('/register', registerRouter);
 app.listen(PORT, () => {
     console.log(`Listening on port: ${PORT}`)
 });
+
