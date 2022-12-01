70f7c721b6981a07c62e9fe3d47604381dbfc5f1
Dylan Gee <dylangee@gmail.com>
2022-12-01 21:58:16 +0000

    Basic log in created

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,13 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { NavLink } from 'react-router-dom';
 
 export default function Home() {
+  const [ userInfo, setUserInfo ] = useState([])
+
+  const getUserInfo = async () => {
+    const data = await fetch('http://localhost:5000');
+    return console.log(data);
+  };
+
   return (
     <>
   <h1>Home</h1>
+  <NavLink to="/login">
+    Login
+  </NavLink>
+  <br />
   <NavLink to="/register">
     Register
   </NavLink>
+  <br />
+  <button onClick={getUserInfo}>User info</button>
   </>
   )
 }
--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -5,6 +5,17 @@ export default function Login() {
   return (
   <>
     <h1>Login</h1>
+    <form action='http://localhost:5000/login' method='POST'>
+      <div>
+      <input type="email" id="email" name="email" placeholder="Email" required />
+      </div>
+      <div>
+      <input type="password" id="password" name="password" placeholder="password" required />
+      </div>
+      <div>
+      <button type="submit">Submit</button>
+      </div>
+    </form>
     <NavLink to='/register'>
       Register
     </NavLink>
